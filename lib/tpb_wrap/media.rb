ef3a71fbc8ec4a842ea38dddd24e2200b45c5ff2
Mike Harris <mharris717@gmail.com>
2013-01-02 13:51:15 -0500

    handle url spaces

--- lib/tpb_wrap/media.rb
+++ lib/tpb_wrap/media.rb
@@ -3,7 +3,8 @@ module TpbWrap
     include FromHash
     attr_accessor :name
     def search_url
-      "http://thepiratebay.se/search/#{name}/0/7/0"
+      n = URI::encode(name)
+      "http://thepiratebay.se/search/#{n}/0/7/0"
     end
     def magnet_url
       Page.new(:url => search_url).first_source.magnet_url

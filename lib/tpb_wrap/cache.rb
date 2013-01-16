f1892a363e9ee1ca3794c73f33dd80036315b74b
Mike Harris <mharris717@gmail.com>
2013-01-16 11:31:56 -0500

    add nocache option

--- lib/tpb_wrap/cache.rb
+++ lib/tpb_wrap/cache.rb
@@ -1,8 +1,17 @@
-def get_cached_body(url)
+def get_cached_body_inner(url)
   require 'open-uri'
   require 'json'
   cache_url = "http://pagecache.herokuapp.com/addresses/ignore.json?url=#{url}&max_age=9999"
   res = open(cache_url).read
   res = JSON::load(res)
   res['body']
+end
+
+def get_cached_body(url)
+  require 'open-uri'
+  if ENV['nocache']
+    open(url).read
+  else
+    get_cached_body_inner(url)
+  end
 end
